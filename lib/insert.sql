3feb78d3f1b5be7f7ae8339988a4187e51a818ae
catherline nyanduko mirobe <catherline.nyanduko@student.moringaschool.com>
2022-08-24 19:20:46 +0300

    commit tree 6f4d76863030a98be8b34f8481d386959f3c2673

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)  VALUES ("Mr Chocolate", 5,"M", "brown", "ok", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)  VALUES ("Rowdy", 6,"F", "blue", "ok", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive)  VALUES ("Tabitha", 5,"F", "blue", "ok", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)  VALUES ("Sergeant Brown", 5,"M", "blue", "sweet", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)  VALUES ("Melissa", 5,"F", "blue", "sweet", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)  VALUES ("Grinch", 5,"F", "blue", "sweet", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive)  VALUES ("Wendy", 5,"F", "blue", "bad", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive)  VALUES (NULL, 5,"F", "blue", "sweet", 1);
+
